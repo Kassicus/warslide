520e9b1dc13bea84045111841fe399d6fb0ef46f
Kason <kason.suchow@gmail.com>
2023-03-13 17:37:55 -0600

    Debug interface is working, now for the real fun

---
+++ debug.py
@@ -0,0 +1,52 @@
+import pygame
+
+import lib
+
+class DebugInterface():
+    def __init__(self):
+        self.active = False
+
+        self.font = pygame.font.SysFont("Courier", 12)
+        self.margin = 10
+
+        # The text element of each debug menu item
+        self.t_fps = None
+        self.t_mouse = None
+
+        # The offset position of each debug menu item
+        self.o_fps = 0
+        self.o_mouse = 0
+
+        self.display_surface = pygame.display.get_surface()
+
+    def get_fps(self, clock: pygame.time.Clock) -> list [pygame.Surface, int]:
+        string = "FPS: " + str(int(clock.get_fps()))
+        text = self.font.render(string, True, lib.color.white)
+
+        offset = int(lib.screen_width - text.get_width() - self.margin)
+
+        return text, offset
+    
+    def get_mouse(self) -> list [pygame.Surface, int]:
+        x, y = pygame.mouse.get_pos()
+
+        string = "Mouse: " + str(x) + "," + str(y)
+        text = self.font.render(string, True, lib.color.white)
+
+        offset = int(lib.screen_width - text.get_width() - self.margin)
+
+        return text, offset
+    
+    def toggle_active(self):
+        if self.active:
+            self.active = False
+        else:
+            self.active = True
+
+    def draw(self):
+        self.display_surface.blit(self.t_fps, (self.o_fps, 10))
+        self.display_surface.blit(self.t_mouse, (self.o_mouse, 30))
+
+    def update(self, clock: pygame.time.Clock):
+        self.t_fps, self.o_fps = self.get_fps(clock)
+        self.t_mouse, self.o_mouse = self.get_mouse()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 import lib
+import debug
 
 pygame.init()
 
@@ -11,6 +12,8 @@ class Game():
 
         self.running = True
         self.clock = pygame.time.Clock()
+
+        self.debug_interface = debug.DebugInterface()
         
     def run(self):
         while self.running:
@@ -29,10 +32,18 @@ class Game():
                 if event.key == pygame.K_ESCAPE:
                     self.running = False
 
+                if event.key == pygame.K_TAB:
+                    self.debug_interface.toggle_active()
+
     def draw(self):
         self.screen.fill(lib.color.black)
+
+        if self.debug_interface.active:
+            self.debug_interface.draw()
         
     def update(self):
+
+        self.debug_interface.update(self.clock)
         pygame.display.update()
         lib.delta_time = self.clock.tick(lib.framerate) / 1000
 
